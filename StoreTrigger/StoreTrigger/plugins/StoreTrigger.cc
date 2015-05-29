b3794b3e6e130535c6f700e98bd6727abd762e07
Douglas Burns <db0268@bristol.ac.uk>
2015-05-29 10:26:40 +0100

    made StoreTrigger.h file

--- StoreTrigger/StoreTrigger/plugins/StoreTrigger.cc
+++ StoreTrigger/StoreTrigger/plugins/StoreTrigger.cc
@@ -22,55 +22,9 @@
 #include <iostream>
 
 // user include files
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
-
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-
-#include "DataFormats/Common/interface/TriggerResults.h"
-#include "FWCore/Common/interface/TriggerNames.h"
-
-//
-// class declaration
-//
-
-class StoreTrigger : public edm::EDAnalyzer {
-   public:
-      explicit StoreTrigger(const edm::ParameterSet&);
-      ~StoreTrigger();
-
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+#include "StoreTrigger.h"
 
 
-   private:
-      virtual void beginJob() override;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
-      virtual void endJob() override;
-
-      const edm::InputTag   hltInputTag_;
-
-      //virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
-      //virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
-      //virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
-      //virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
-
-      // ----------member data ---------------------------
-};
-
-//
-// constants, enums and typedefs
-//
-
-//
-// static data member definitions
-//
-
-//
-// constructors and destructor
-// up to heere can go in .h file?
 StoreTrigger::StoreTrigger(const edm::ParameterSet& iConfig) :
     hltInputTag_(iConfig.getParameter < edm::InputTag > ("HLTInputTag"))
 {
@@ -159,15 +113,9 @@ StoreTrigger::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
     std::cout << j << "th Trig index : " << TrigNames.triggerIndex(pathNames.at(j)) << std::endl;
 
-
-
     // Check if it passes
     bool passTrig=triggerResults->accept(TrigNames.triggerIndex(pathNames.at(j)));  
     std::cout << "pass trigger : " << passTrig << std::endl;
-
-
-
-
 
   }
 
---
+++ StoreTrigger/StoreTrigger/plugins/StoreTrigger.h
@@ -0,0 +1,50 @@
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "FWCore/Common/interface/TriggerNames.h"
+
+//
+// class declaration
+//
+
+class StoreTrigger : public edm::EDAnalyzer {
+   public:
+      explicit StoreTrigger(const edm::ParameterSet&);
+      ~StoreTrigger();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() override;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+      virtual void endJob() override;
+
+      const edm::InputTag   hltInputTag_;
+
+      //virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
+      //virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
+      //virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+      //virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+
+      // ----------member data ---------------------------
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+// up to heere can go in .h file?
